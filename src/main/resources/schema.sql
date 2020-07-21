47e834a349bf9051d9f3c5a5f410ff83ee90e325
sasaki-jcsc <sasaki@jcsc.co.jp>
2020-07-21 23:11:14 +0900

    updated test data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,7 @@
-drop table if exists group_tbl;
-create table group_tbl (
-    id char(36) not null,
-    name varchar(256) not null,
+drop table if exists item_tbl;
+create table item_tbl (
+    `id` int not null,
+    `name` varchar(256) not null,
+    `size` decimal(3, 2),
     primary key(id)
 );
-
-drop table if exists user_tbl;
-create table user_tbl (
-    id char(36) not null,
-    name varchar(256) not null,
-    primary key(id)
- );
-
-drop table if exists group_user_tbl;
-create table group_user_tbl (
-    group_id char(36) not null,
-    user_id char(36) not null,
-    primary key(group_id, user_id)
-);
-create index on group_user_tbl (user_id, group_id);
